777046a6e69c57ec86e9d15d1e3771bab74866e2
A_lie <73252601+alie88dova@users.noreply.github.com>
2024-12-23 15:40:42 +0300

    func to get info about game by id

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import re
+
 import requests
 from parser.parser import get_all_games_ids
 
 
 if __name__ == '__main__':
-    x = get_all_games_ids()
+    pass
---
+++ test/test.py
@@ -0,0 +1,6 @@
+from parser.parser import get_info
+
+
+if __name__ == '__main__':
+    x = get_info(1234)
+    print(x)
